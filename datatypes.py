2d89dc5636fc498db512c62ebd4f07bebdd4f6cd
Cyndrome <mashrafi.mohammad@gmail.com>
2026-03-11 10:53:18 +0600

    Uploaded data types examples and exercises

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+""""
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+name = " Hailey "
+name = name.strip()
+print("\t" + name + "\n" + "Hello there")
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+print(5+3)
+print(18-10)
+print(4*2)
+print(int(8/1))
+print(2**3)
+"""
